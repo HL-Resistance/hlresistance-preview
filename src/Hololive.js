c90df368a2be93ff83bf86b33b23d0d61b555ba2
Aeora <47289732+Aeoraa@users.noreply.github.com>
2020-06-23 18:21:35 +0800

    migration to react
    Migrate codes to react
    skeleton of components with fillers are tested

---
+++ src/Hololive.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+//filler content
+export const About = () => (
+    <div>
+        <h2>Hello About</h2>
+    </div>
+)
---
+++ src/Translation.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+//filler content
+export const Translation = () => (
+    <div>
+        <h2>Hello Translation</h2>
+    </div>
+)
