61d6a3fa9250120d61b9dd38e52fd9fc7372ace9
Aeora <47289732+Aeoraa@users.noreply.github.com>
2020-06-23 22:47:50 +0800

    Remove build from gitignore
    push build folder to github

---
+++ build/precache-manifest.e52fd1db8dd35d0e97c24055dd30ce6c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cec066da9335ecf9bc7deb29c31e2265",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b727811c523112d4dccd",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3e88d3ca791cb7c59ec0",
+    "url": "/static/css/main.c05ef92f.chunk.css"
+  },
+  {
+    "revision": "b727811c523112d4dccd",
+    "url": "/static/js/2.6084586d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6084586d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e88d3ca791cb7c59ec0",
+    "url": "/static/js/main.2d8b0319.chunk.js"
+  },
+  {
+    "revision": "102c0a19fb6125544a97",
+    "url": "/static/js/runtime-main.2bd094c5.js"
+  },
+  {
+    "revision": "24a8040309dee61541b6bab2467f2091",
+    "url": "/static/media/Logo.24a80403.png"
+  }
+]);
