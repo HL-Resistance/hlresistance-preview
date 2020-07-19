47eb5788f093209575e6a088424ce89fc88d33c2
GoldElysium <48455312+GoldElysium@users.noreply.github.com>
2020-07-19 23:30:21 +0200

    Remove unnecessary imports and tag

--- pages/creative.js
+++ pages/creative.js
@@ -1,13 +1,8 @@
-import React from 'react'
-import Layout from "../components/Layout";
-
 //filler content
 export default function Creative() {
     return (
-        <Layout>
-            <div>
-                <h2>Hello creative</h2>
-            </div>
-        </Layout>
+        <div>
+            <h2>Hello creative</h2>
+        </div>
     )
 }
